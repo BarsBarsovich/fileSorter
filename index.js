1a595d85f5ad00f32dc3b1b170aed85588896009
barspredator <boris.dryabin@gmail.com>
2019-06-11 15:02:29 +0300

    task finished

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 module.exports = require('./eslintrc.json')
+var fs = require('fs');
+var path = require('path');
+
+const copyFiles = () => {
+  console.log(filesList);
+  filesList.forEach(file => {
+    if (file.key === undefined) {
+      return;
+    }
+
+    console.log('FileName', file.key);
+    const firstFileLetter = file.key.charAt(0);
+    fs.exists(firstFileLetter, (err, folder) => {
+      if (!folder) {
+        fs.mkdir(firstFileLetter, (err, data) => {
+          // теперь надо перенести файлы в нашу директорию
+          fs.rename(file.value, firstFileLetter+'\\' + file.key, (err)=>{
+            if (err){
+              console.log(err);
+              process.exit(err);
+            }
+          })
+        })
+      }
+    });
+  })
+}
+
+const parseDir = (dirName) => {
+  const files = fs.readdirSync(dirName);  
+  files.forEach(fileItem => {
+    const fileFullName = path.join(dirName, fileItem);
+    let state = fs.statSync(fileFullName);
+    if (state.isDirectory()) {      
+      parseDir(fileFullName);
+    } else {      
+      const relativePath = path.resolve(process.cwd() + '\\' + dirName, fileItem);
+      filesList.push({ key: fileItem, value: relativePath });
+    }
+  });
+  copyFiles();
+}
+
+const filePath = 'filesTraining';
+const args = process.argv;
+let filesList = [{}];
+parseDir(filePath);
