af9dbfc861fb05b7d288071906c4c340a1baf43d
barspredator <boris.dryabin@gmail.com>
2019-06-13 23:46:28 +0300

    Добавил структуру для своего проекта

--- index.js
+++ index.js
@@ -1,48 +1,80 @@
 module.exports = require('./eslintrc.json')
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
+const yargs = require('yargs');
+const args = yargs
+  .usage('Usage $0 [options]')
+  .help('help')
+  .alias('help', 'h')
+  .version('0.0.1')
+  .alias('version', 'v')
+  .example('$0 --entry filesDir --output dist', '--> Example for runing my app')
+  .option('entry', {
+    alias: 'e',
+    describe: 'Source directory for searh',
+    demandOption: true
+  })
+  .option('delete', {
+    alias: 'd',
+    describe: 'Delete source dir, not implemented yet',
+    demandOption: false
+  })
+  .option('output', {
+    alias: 'o',
+    describe: 'Destination dir',
+    demandOption: true
+  })
+  .epilog('First home work')
+  .argv
 
 const copyFiles = () => {
   console.log(filesList);
   filesList.forEach(file => {
-    if (file.key === undefined) {
-      return;
-    }
-
-    console.log('FileName', file.key);
     const firstFileLetter = file.key.charAt(0);
     fs.exists(firstFileLetter, (err, folder) => {
       if (!folder) {
-        fs.mkdir(firstFileLetter, (err, data) => {
+        fs.mkdir(path.join(destinationDir,firstFileLetter), (err, data) => {
           // теперь надо перенести файлы в нашу директорию
-          fs.rename(file.value, firstFileLetter+'\\' + file.key, (err)=>{
-            if (err){
+          fs.copyFile(file.value, destinationDir + '\\'+ firstFileLetter + '\\' + file.key, (err) => {
+            if (err) {
               console.log(err);
               process.exit(err);
             }
-          })
-        })
+          });
+        });
       }
     });
   })
 }
 
 const parseDir = (dirName) => {
-  const files = fs.readdirSync(dirName);  
-  files.forEach(fileItem => {
-    const fileFullName = path.join(dirName, fileItem);
-    let state = fs.statSync(fileFullName);
-    if (state.isDirectory()) {      
-      parseDir(fileFullName);
-    } else {      
-      const relativePath = path.resolve(process.cwd() + '\\' + dirName, fileItem);
-      filesList.push({ key: fileItem, value: relativePath });
-    }
-  });
-  copyFiles();
+  try {
+    const files = fs.readdirSync(dirName);
+    files.forEach(fileItem => {
+      const fileFullName = path.join(dirName, fileItem);
+      let state = fs.statSync(fileFullName);
+      if (state.isDirectory()) {
+        parseDir(fileFullName);
+      } else {
+        const relativePath = path.resolve(process.cwd() + '\\' + dirName, fileItem);
+        filesList.push({ key: fileItem, value: relativePath });
+      }
+    });
+  } catch (err) {
+    console.err('Errror occured');
+    console.log(err);
+    // process.exit(err);    
+  }
 }
 
-const filePath = 'filesTraining';
-const args = process.argv;
-let filesList = [{}];
-parseDir(filePath);
+const sourceDir = args.e;
+const destinationDir = args.o;
+let filesList = [];
+parseDir(sourceDir);
+fs.mkdir(destinationDir, (err, data) => {
+  if (err) {
+    console.log(err);
+    process.exit(err);
+  }
+  copyFiles();
+})
